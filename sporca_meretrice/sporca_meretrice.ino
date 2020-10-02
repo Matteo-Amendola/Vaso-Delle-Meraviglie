44576217b29dba2c37095f2f41b16ce54d07b39c
Matteo-Amendola <matteoamendola.27@gmail.com>
2020-10-02 08:23:21 +0200

    sporca_meretrice_2.0

--- sporca_meretrice/sporca_meretrice.ino
+++ sporca_meretrice/sporca_meretrice.ino
@@ -1,48 +1,58 @@
 #include <LiquidCrystal.h>
+#include "DHT.h"
+
+#define DHTPIN 8
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-int val_cad = 0; // valore della conversione analogico digitale
-float f_temperatura = 0;
+byte newChar[8] = {
+        B00110,
+        B01001,
+        B01001,
+        B00110,
+        B00000,
+        B00000,
+        B00000,
+        B00000
+};
 
 void setup() {
-  Serial.begin(9600);
-  analogReference(EXTERNAL); // indichiamo al converrtitore AD che deve impiegare la tensione su AREF come valore di riferimento per la conversione
-  lcd.begin(16, 2); // impostiamo numero di colonne e righe 
+  // Serial.begin(9600);
+  // analogReference(EXTERNAL); // indichiamo al converrtitore AD che deve impiegare la tensione su AREF come valore di riferimento per la conversione
+  lcd.createChar(0, newChar);
+  lcd.begin(16, 2); // impostiamo numero di colonne e righe
+  dht.begin();
 }
 
 void loop() {
   lcd.print("CIAONE!");
-  delay(2000);
-
-  for(int L=0; L<10000; L++) {
-    lcd.clear();
   
-    for(byte TheCycle; TheCycle<100; TheCycle++){
-      val_cad += analogRead(0);
-    }
-
-    val_cad /= 100; // eseguiamo la media dei 100 valori
-    f_temperatura = ((val_cad * 0.0032) - 0.5) / 0.01; // calcoliamo la temperatura in °C
+  delay(3000);
+  lcd.clear();
+  delay(300);
   
-    lcd.print("Temperatura:");
+  int t = dht.readTemperature();
+  int u = dht.readHumidity();
   
-    lcd.setCursor(0,1);
-  
-    lcd.print(f_temperatura,1);
-    lcd.print("C");
+  lcd.print("Temperatura : ");
+  lcd.setCursor(0, 1);
+  lcd.print(t);
+  lcd.write((int)0);
+  lcd.print("C");
 
-    delay(5000);
+  delay(5000);
+  lcd.clear();
+  delay(300);
 
-    if(L >= 999) L=0;
-  }
+  lcd.print("Umidita' : ");
+  lcd.setCursor(0, 1);
+  lcd.print(u);
+  lcd.print("%");
 
-   
-    
-/*  for(int i=0; i<122; i++){
-    delay(500);
-    lcd.print(i); // stampiamo il numero di secondi dall'ultimo reset
-    Serial.print(i);
-  }
-*/
+  delay(5000);
+  lcd.clear();
+  delay(300);
 }
